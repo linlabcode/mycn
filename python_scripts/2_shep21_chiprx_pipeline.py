a896b7e7d60f38d8eed36709c13aaac51b54e336
charlesylin <charles.y.lin@gmail.com>
2017-01-11 09:21:15 -0600

    chiprx code

--- python_scripts/2_shep21_chiprx_pipeline.py
+++ python_scripts/2_shep21_chiprx_pipeline.py
@@ -165,7 +165,7 @@ def main():
     #this usually takes ~2-3 hours on a reasonably fast machine
     #a 3 hour time out on this entire operation is set
     #if peak calling takes longer than 3 hours, simply run the script again after completion
-    #run_macs(shep21_chiprx_dataFile)
+    run_macs(shep21_chiprx_dataFile)
 
 
     print('\n\n')
@@ -174,8 +174,8 @@ def main():
     print('#======================================================================')
     print('\n\n')
 
-    #scale_factor_path = '%sHG19_SHEP21_CHIPRX_SCALE_FACTORS.txt' % (tableFolder)
-    #scale_factor_table = writeScaleFactors(shep21_chiprx_dataFile,namesList=[],output=scale_factor_path)
+    scale_factor_path = '%sHG19_SHEP21_CHIPRX_SCALE_FACTORS.txt' % (tableFolder)
+    scale_factor_table = writeScaleFactors(shep21_chiprx_dataFile,namesList=[],output=scale_factor_path)
 
 
     print('\n\n')
@@ -204,10 +204,187 @@ def main():
     print('\n\n')
 
     #making the intersect and union sets of regions for CTCF and H3K4ME3
-    
+    print('Making stitched intersect and union regions for SHEP21_CTXF_RX')
+    makeStitchedGFF(shep21_chiprx_dataFile,'SHEP21_CTCF_RX',['SHEP21_0HR_CTCF_RX','SHEP21_2HR_CTCF_RX','SHEP21_24HR_CTCF_RX'])
+
+    #print('Making stitched intersect and union regions for SHEP21_H3K4ME3_RX')
+    makeStitchedGFF(shep21_chiprx_dataFile,'SHEP21_H3K4ME3_RX',['SHEP21_0HR_H3K4ME3_RX','SHEP21_2HR_H3K4ME3_RX','SHEP21_24HR_H3K4ME3_RX'])
+
     #making the +/- 1kb active TSS gff
+    print('Making active gene tss gff')
+    gene_list_path = '%sHG19_NB_H3K27AC_ACTIVE_UNION.txt' % (geneListFolder)
+    make_tss_gff(gene_list_path,'NB_H3K27AC_ACTIVE_UNION')
 
     #taking mycn sites and subdividing by promoters and enhancers and adding +/- 500 flank
+    mycn_stats_path = '%sHG19_NB_MYCN_CONSERVED_STATS_TABLE.txt' % (tableFolder)
+    make_mycn_gffs(mycn_stats_path,window=0)
+    make_mycn_gffs(mycn_stats_path,window=500)
+
+
+    print('\n\n')
+    print('#======================================================================')
+    print('#======================VII. MAPPING TO REGIONS=========================')
+    print('#======================================================================')
+    print('\n\n')
+
+    #mapping ctcf to ctcf regions
+    gffList = ['%sHG19_SHEP21_CTCF_RX_UNION_-0_+0.gff' % (gffFolder), '%sHG19_SHEP21_CTCF_RX_INTERSECT_-0_+0.gff' % (gffFolder)]
+    names_list = ['SHEP21_0HR_CTCF_RX','SHEP21_2HR_CTCF_RX','SHEP21_24HR_CTCF_RX','SHEP21_0HR_INPUT_RX_2','SHEP21_2HR_INPUT_RX_2','SHEP21_24HR_INPUT_RX_2']
+    map_regions(shep21_chiprx_dataFile,gffList,names_list)
+
+    #mapping h3k4me3 to h3k4me3 regions
+    gffList = ['%sHG19_SHEP21_H3K4ME3_RX_UNION_-0_+0.gff' % (gffFolder), '%sHG19_SHEP21_H3K4ME3_RX_INTERSECT_-0_+0.gff' % (gffFolder)]
+    names_list = ['SHEP21_0HR_H3K4ME3_RX','SHEP21_2HR_H3K4ME3_RX','SHEP21_24HR_H3K4ME3_RX','SHEP21_0HR_INPUT_RX_2','SHEP21_2HR_INPUT_RX_2','SHEP21_24HR_INPUT_RX_2']
+    map_regions(shep21_chiprx_dataFile,gffList,names_list)
+    
+    #mapping everybody to active TSS locations
+    gffList = ['%sHG19_TSS_NB_H3K27AC_ACTIVE_UNION_-1000_+1000.gff' % (gffFolder)]
+    map_regions(shep21_chiprx_dataFile,gffList,names_list=[])
+
+
+    #mapping everybody to mycn peaks
+    gffList = ['%sHG19_TSS_NB_H3K27AC_ACTIVE_UNION_-1000_+1000.gff' % (gffFolder)]
+    gffList = ['%sHG19_NB_MYCN_CONSERVED_-0_+0.gff' % (gffFolder),
+               '%sHG19_NB_MYCN_CONSERVED_-500_+500.gff' % (gffFolder),
+               '%sHG19_NB_MYCN_CONSERVED_ENHANCER_-0_+0.gff' % (gffFolder),
+               '%sHG19_NB_MYCN_CONSERVED_ENHANCER_-500_+500.gff' % (gffFolder),
+               '%sHG19_NB_MYCN_CONSERVED_PROMOTER_-0_+0.gff' % (gffFolder),
+               '%sHG19_NB_MYCN_CONSERVED_PROMOTER_-500_+500.gff' % (gffFolder),
+               ]
+    map_regions(shep21_chiprx_dataFile,gffList,names_list=[])
+
+
+
+    print('\n\n')
+    print('#======================================================================')
+    print('#==================VIII. MAKING CHIPRX BOX PLOTS=======================')
+    print('#======================================================================')
+    print('\n\n')
+
+    #making boxplots
+    boxplot_script_path = '%sr_scripts/4_chiprx_plots.R' % (projectFolder)
+    scale_table_path = '%sHG19_SHEP21_CHIPRX_SCALE_FACTORS.txt' % (tableFolder)
+
+    #=============================================================================
+    #for ctcf intersect
+    names_string = 'SHEP21_0HR_CTCF_RX,SHEP21_2HR_CTCF_RX,SHEP21_24HR_CTCF_RX'
+    background_string = 'SHEP21_0HR_INPUT_RX_2,SHEP21_2HR_INPUT_RX_2,SHEP21_24HR_INPUT_RX_2'
+    plot_name = 'SHEP21_CTCF_RX_INTERSECT'
+    signal_table_path = '%sHG19_SHEP21_CTCF_RX_INTERSECT_-0_+0_SIGNAL.txt' % (signalFolder)
+    
+    r_cmd = 'Rscript %s %s %s %s %s %s %s' % (boxplot_script_path,signal_table_path,scale_table_path,names_string,background_string,plot_name,projectFolder)
+    print(r_cmd)
+    os.system(r_cmd)
+
+
+    #=============================================================================
+    #for ctcf union
+    names_string = 'SHEP21_0HR_CTCF_RX,SHEP21_2HR_CTCF_RX,SHEP21_24HR_CTCF_RX'
+    background_string = 'SHEP21_0HR_INPUT_RX_2,SHEP21_2HR_INPUT_RX_2,SHEP21_24HR_INPUT_RX_2'
+    plot_name = 'SHEP21_CTCF_RX_UNION'
+    signal_table_path = '%sHG19_SHEP21_CTCF_RX_UNION_-0_+0_SIGNAL.txt' % (signalFolder)
+    
+    r_cmd = 'Rscript %s %s %s %s %s %s %s' % (boxplot_script_path,signal_table_path,scale_table_path,names_string,background_string,plot_name,projectFolder)
+    print(r_cmd)
+    os.system(r_cmd)
+
+
+
+    #=============================================================================
+    #for h3k4me3 intersect
+    names_string = 'SHEP21_0HR_H3K4ME3_RX,SHEP21_2HR_H3K4ME3_RX,SHEP21_24HR_H3K4ME3_RX'
+    background_string = 'SHEP21_0HR_INPUT_RX_2,SHEP21_2HR_INPUT_RX_2,SHEP21_24HR_INPUT_RX_2'
+    plot_name = 'SHEP21_H3K4ME3_RX_INTERSECT'
+    signal_table_path = '%sHG19_SHEP21_H3K4ME3_RX_INTERSECT_-0_+0_SIGNAL.txt' % (signalFolder)
+    
+    r_cmd = 'Rscript %s %s %s %s %s %s %s' % (boxplot_script_path,signal_table_path,scale_table_path,names_string,background_string,plot_name,projectFolder)
+    print(r_cmd)
+    os.system(r_cmd)
+
+
+    #=============================================================================
+    #for h3k4me3 union
+    names_string = 'SHEP21_0HR_H3K4ME3_RX,SHEP21_2HR_H3K4ME3_RX,SHEP21_24HR_H3K4ME3_RX'
+    background_string = 'SHEP21_0HR_INPUT_RX_2,SHEP21_2HR_INPUT_RX_2,SHEP21_24HR_INPUT_RX_2'
+    plot_name = 'SHEP21_H3K4ME3_RX_UNION'
+    signal_table_path = '%sHG19_SHEP21_H3K4ME3_RX_UNION_-0_+0_SIGNAL.txt' % (signalFolder)
+    
+    r_cmd = 'Rscript %s %s %s %s %s %s %s' % (boxplot_script_path,signal_table_path,scale_table_path,names_string,background_string,plot_name,projectFolder)
+    print(r_cmd)
+    os.system(r_cmd)
+
+    #=============================================================================
+    #for nb mycn enhancers
+    dataDict=  pipeline_dfci.loadDataTable(shep21_chiprx_dataFile)
+    set_name = 'MYCN'
+    names_list = [name for name in dataDict.keys() if name.count(set_name) > 0]
+    names_list.sort()
+    background_list = [ dataDict[name]['background'] for name in names_list]
+    names_string = ','.join(names_list)
+    background_string = ','.join(background_list)
+
+    plot_name = 'NB_MYCN_CONSERVED_ENHANCER_-500_+500'
+    signal_table_path = '%sHG19_%s_SIGNAL.txt' % (signalFolder,plot_name)
+    
+    r_cmd = 'Rscript %s %s %s %s %s %s %s' % (boxplot_script_path,signal_table_path,scale_table_path,names_string,background_string,plot_name,projectFolder)
+    print(r_cmd)
+    
+    os.system(r_cmd)
+
+    #=============================================================================
+    #for nb mycn enhancers
+    dataDict=  pipeline_dfci.loadDataTable(shep21_chiprx_dataFile)
+    set_name = 'MYCN'
+    names_list = [name for name in dataDict.keys() if name.count(set_name) > 0]
+    names_list.sort()
+    background_list = [ dataDict[name]['background'] for name in names_list]
+    names_string = ','.join(names_list)
+    background_string = ','.join(background_list)
+
+    plot_name = 'NB_MYCN_CONSERVED_ENHANCER_-0_+0'
+    signal_table_path = '%sHG19_%s_SIGNAL.txt' % (signalFolder,plot_name)
+    
+    r_cmd = 'Rscript %s %s %s %s %s %s %s' % (boxplot_script_path,signal_table_path,scale_table_path,names_string,background_string,plot_name,projectFolder)
+    print(r_cmd)
+    
+    os.system(r_cmd)
+
+    #=============================================================================
+    #for nb mycn promoter +/- 500bp
+    dataDict=  pipeline_dfci.loadDataTable(shep21_chiprx_dataFile)
+    set_name = 'MYCN'
+    names_list = [name for name in dataDict.keys() if name.count(set_name) > 0]
+    names_list.sort()
+    background_list = [ dataDict[name]['background'] for name in names_list]
+    names_string = ','.join(names_list)
+    background_string = ','.join(background_list)
+
+    plot_name = 'NB_MYCN_CONSERVED_PROMOTER_-500_+500'
+    signal_table_path = '%sHG19_%s_SIGNAL.txt' % (signalFolder,plot_name)
+    
+    r_cmd = 'Rscript %s %s %s %s %s %s %s' % (boxplot_script_path,signal_table_path,scale_table_path,names_string,background_string,plot_name,projectFolder)
+    print(r_cmd)
+    
+    os.system(r_cmd)
+
+    #=============================================================================
+    #for nb mycn promoter +/- 0
+    dataDict=  pipeline_dfci.loadDataTable(shep21_chiprx_dataFile)
+    set_name = 'MYCN'
+    names_list = [name for name in dataDict.keys() if name.count(set_name) > 0]
+    names_list.sort()
+    background_list = [ dataDict[name]['background'] for name in names_list]
+    names_string = ','.join(names_list)
+    background_string = ','.join(background_list)
+
+    plot_name = 'NB_MYCN_CONSERVED_PROMOTER_-0_+0'
+    signal_table_path = '%sHG19_%s_SIGNAL.txt' % (signalFolder,plot_name)
+    
+    r_cmd = 'Rscript %s %s %s %s %s %s %s' % (boxplot_script_path,signal_table_path,scale_table_path,names_string,background_string,plot_name,projectFolder)
+    print(r_cmd)
+    
+    os.system(r_cmd)
+
 
 
 #==========================================================================
@@ -370,6 +547,7 @@ def scaleWiggles(dataFile,scaleTableFile,names_list=[]):
         cmd = 'gzip %s' % (wig_path)
         print(cmd)
         os.system(cmd)
+    os.chdir(projectFolder)
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #~~~~~~~~~~~~~~~~~~~~~~~~~FILTERING PEAKS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -410,6 +588,9 @@ def filterPeaks(dataFile,maskFolder,macsEnrichedFolder,namesList = [],repeatList
         ticker = 0
         peak_path = '%s%s' % (macsEnrichedFolder,dataDict[name]['enrichedMacs'])
         filtered_path = string.replace(peak_path,'.bed','_filtered.bed')
+        if utils.checkOutput(filtered_path,0,0):
+            print('Filtered output identified for %s at %s' % (name, filtered_path))
+            continue
         filtered_bed = []
         peak_bed = utils.parseTable(peak_path,'\t')
 
@@ -456,8 +637,6 @@ def filterPeaks(dataFile,maskFolder,macsEnrichedFolder,namesList = [],repeatList
 #~~~~~~~~~~~~~~~~~~~~~~~~~MAKING REGION GFFS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-
-
 def makeStitchedGFF(dataFile,set_name,names_list):
 
     '''
@@ -470,7 +649,7 @@ def makeStitchedGFF(dataFile,set_name,names_list):
     collection_dict = {}
     for name in names_list:
         print(name)
-        macsEnrichedFile = '%s%s' % (macsEnrichedFolder,dataDict[name]['enrichedMacs'])
+        macsEnrichedFile = '%s%s_peaks_filtered.bed' % (macsEnrichedFolder,name)
         collection = utils.importBoundRegion(macsEnrichedFile,name)
         collection_dict[name]=collection
         loci+= collection.getLoci()
@@ -480,7 +659,7 @@ def makeStitchedGFF(dataFile,set_name,names_list):
 
     gff = utils.locusCollectionToGFF(stitched_collection)
 
-    out_path = '%sHG19_%s_ALL_-0_+0.gff' % (gffFolder,set_name)
+    out_path = '%sHG19_%s_UNION_-0_+0.gff' % (gffFolder,set_name)
     print(out_path)
     utils.unParseTable(gff,out_path,'\t')
 
@@ -513,6 +692,101 @@ def makeStitchedGFF(dataFile,set_name,names_list):
     intersect_path = '%sHG19_%s_INTERSECT_-0_+0.gff' % (gffFolder,set_name)
     print(intersect_path)
     utils.unParseTable(intersect_gff,intersect_path,'\t')
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~MAKING ACTIVS TSS GFFS~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+def make_tss_gff(gene_list_path,name):
+
+    '''
+    makes a +/- 1kb tss gff from the provided gene list
+    '''
+
+    tss_gff = utils.parseTable('%sHG19_TSS_ALL_-1000_+1000.gff' % (gffFolder),'\t')
+
+    gene_list_table = utils.parseTable(gene_list_path,'\t')
+
+    gene_rows = [int(line[0]) - 1 for line in gene_list_table]
+
+    row_gff =[tss_gff[row] for row in gene_rows]
+
+    row_gff_path = '%sHG19_TSS_%s_-1000_+1000.gff' % (gffFolder,name)
+
+    utils.unParseTable(row_gff,row_gff_path,'\t')
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~MAKING MYCN PROMOTER AND ENHANCER REGIONS~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+def make_mycn_gffs(mycn_stats_path,window=0):
+
+    '''
+    makes promoter and enhancer gffs from the mycn stats table
+    with an appropriate flanking window
+    returns paths
+    '''
+
+    window = int(window)
+
+    mycn_stats_table = utils.parseTable(mycn_stats_path,'\t')
+    
+    enhancer_gff = []
+    promoter_gff = []
+    for line in mycn_stats_table[1:]:
+        
+        gff_line = [line[1],line[0],'',int(line[2]) - window,int(line[3]) + window,'','.','',line[0]]
+
+        if int(line[5]) == 1:
+            promoter_gff.append(gff_line)
+        
+        if int(line[6]) == 1:
+            enhancer_gff.append(gff_line)
+
+    enhancer_gff_path = '%sHG19_NB_MYCN_CONSERVED_ENHANCER_-%s_+%s.gff' % (gffFolder,window,window)
+    promoter_gff_path = '%sHG19_NB_MYCN_CONSERVED_PROMOTER_-%s_+%s.gff' % (gffFolder,window,window)
+    utils.unParseTable(promoter_gff,promoter_gff_path,'\t')
+    utils.unParseTable(enhancer_gff,enhancer_gff_path,'\t')
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~MAPPING CHIPRX TO REGIONS~~~~~~~~~~~~~~~~~~~~~~~~
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+def map_regions(nb_all_chip_dataFile,gffList,names_list=[]):
+
+    '''
+    making a normalized binding signal table at all regions
+    '''
+
+    #since each bam has different read lengths, important to carefully normalize quantification
+    dataDict = pipeline_dfci.loadDataTable(nb_all_chip_dataFile)
+
+    if len(names_list) == 0:
+        names_list = dataDict.keys()
+    names_list.sort()
+
+    
+    for name in names_list:
+        bam = utils.Bam(dataDict[name]['bam'])
+        read_length = bam.getReadLengths()[0]
+        bam_extension = 200-read_length
+        print('For dataset %s using an extension of %s' % (name,bam_extension))
+        pipeline_dfci.mapBamsBatch(nb_all_chip_dataFile,gffList,mappedFolder,overWrite =False,namesList = [name],extension=bam_extension,rpm=True)
+
+        
+
+    #want a signal table of all datasets to each gff
+    print('Writing signal tables for each gff:')
+    for gffFile in gffList:
+        gffName = gffFile.split('/')[-1].split('.')[0]
+        signal_table_path = '%s%s_SIGNAL.txt' % (signalFolder,gffName)
+        print(signal_table_path)
+        pipeline_dfci.makeSignalTable(nb_all_chip_dataFile,gffFile,mappedFolder,namesList = names_list,medianNorm=False,output =signal_table_path)
 
 
 
